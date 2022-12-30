b795dc39ebbed2e33b03a69047a78d08d1ffbc19
junho <kode15333@gmail.com>
2022-12-30 23:32:16 +0900

    chore: jest 세팅(config, setup) 패키지로 공통화 처리

---
+++ packages/settings/jest.config.js
@@ -0,0 +1,12 @@
+const commonConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/pages/(.*)$': '<rootDir>/pages/$1',
+        '^@/components/(.*)$': '<rootDir>/src/components/$1',
+        '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+    },
+    testEnvironment: 'jest-environment-jsdom',
+    modulePaths: ['<rootDir>/src'],
+}
+
+module.exports = commonConfig
