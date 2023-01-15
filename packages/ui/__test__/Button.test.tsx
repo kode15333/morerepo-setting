3b027ddedf78a97ec832e63df5036399226d825f
junho <kode15333@gmail.com>
2023-01-15 23:35:45 +0900

    chore: test case update

--- packages/ui/__test__/Button.test.tsx
+++ packages/ui/__test__/Button.test.tsx
@@ -1,10 +1,11 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {Button} from "~/Button";
 
 describe('Button render', function () {
     it('should render correctly', function () {
-        const wrapper = render(<Button/>);
-        expect(wrapper).toMatchSnapshot();
+        render(<Button/>);
+
+        expect(screen.getByText('Boop')).toBeTruthy();
     })
 
 });
