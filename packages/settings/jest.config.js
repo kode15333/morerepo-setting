5af66901abcf158e505a4a08bae241eae20db090
junho <kode15333@gmail.com>
2023-02-27 23:30:16 +0900

    test: report9

--- packages/settings/jest.config.js
+++ packages/settings/jest.config.js
@@ -1,14 +1,22 @@
 const commonConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-        '^@/pages/(.*)$': '<rootDir>/pages/$1',
-        '^@/components/(.*)$': '<rootDir>/src/components/$1',
-        '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  modulePaths: ["<rootDir>/src"],
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
-    testEnvironment: 'jest-environment-jsdom',
-    modulePaths: ['<rootDir>/src'],
-    coverageReporters: ['text', 'lcov'],
-    coverageDirectory: '<rootDir>/coverage',
-}
+  },
+};
 
-module.exports = commonConfig
+module.exports = commonConfig;
