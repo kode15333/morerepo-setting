24a3f0712de91ae2ed06c7b5a79600fc2e9fb84b
junho <kode15333@gmail.com>
2023-02-27 23:43:28 +0900

    test: report9

--- packages/settings/jest.config.js
+++ packages/settings/jest.config.js
@@ -9,14 +9,6 @@ const commonConfig = {
   modulePaths: ["<rootDir>/src"],
   coverageReporters: ["text", "lcov"],
   coverageDirectory: "<rootDir>/coverage",
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
 
 module.exports = commonConfig;
