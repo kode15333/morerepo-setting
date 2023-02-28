dc2315ab6604cc4ff37fbf8e00ea80ef648f0eb1
Junho <50912829+kode15333@users.noreply.github.com>
2023-03-01 00:46:40 +0900

    chore: coverage report (#4)
    * chore: coverage report
    
    * test: report
    
    * test: report
    
    * test: report
    
    * test: report
    
    * test: report
    
    * test: report
    
    * test: report2
    
    * test: report3
    
    * test: report4
    
    * test: report5
    
    * test: report5
    
    * test: report6
    
    * test: report6
    
    * test: report6
    
    * test: report7
    
    * test: report7
    
    * test: report7
    
    * test: report7
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report9
    
    * test: report10
    
    * test: report10
    
    * test: report11
    
    * remove lcov-result-merger
    
    * chore: 수정
    
    * chore: 수정

--- packages/settings/jest.config.js
+++ packages/settings/jest.config.js
@@ -1,13 +1,14 @@
 const commonConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-        '^@/pages/(.*)$': '<rootDir>/pages/$1',
-        '^@/components/(.*)$': '<rootDir>/src/components/$1',
-        '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
-    },
-    testEnvironment: 'jest-environment-jsdom',
-    modulePaths: ['<rootDir>/src'],
-    coverageReporters: ['json-summary', 'text', 'lcov'],
-}
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  modulePaths: ["<rootDir>/src"],
+  coverageReporters: ["json", "lcov", ["text", { skipFull: true }]],
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}"],
+};
 
-module.exports = commonConfig
+module.exports = commonConfig;
