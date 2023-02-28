5b3d72a7b65a825fc00a3879cc12c421ac31199e
junho <kode15333@gmail.com>
2023-03-01 01:48:19 +0900

    chore: index page

--- apps/docs/__test__/Test.test.tsx
+++ apps/docs/__test__/Test.test.tsx
@@ -1,15 +1,13 @@
-import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Home from "../pages";
 
+describe("MSW Test", function () {
+  it("should render", async function () {
+    render(<Home />);
+    const button = screen.getByRole("button");
+    expect(screen.findByText("False")).toBeTruthy();
+    fireEvent.click(button);
 
-describe('MSW Test', function () {
-
-    it('should render', async function () {
-        render(<Home/>);
-        const button = screen.getByRole('button');
-        expect(screen.findByText('False')).toBeTruthy();
-        fireEvent.click(button);
-
-        await waitFor(() => expect(screen.findByText('True')).toBeTruthy())
-    })
+    await waitFor(() => expect(screen.findByText("True")).toBeTruthy());
+  });
 });
--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,30 +1,5 @@
-import {useState} from "react";
-import axios from "axios";
-import {Review} from "../msw/types";
+const Home = () => {
+  return <div>Home</div>;
+};
 
-export default function Home() {
-    const [reviews, setReviews] = useState<Review[] | null>(null)
-    const handleGetReviews = async () => {
-        const {data} = await axios('/reviews')
-        setReviews(data)
-
-    }
-
-    return (
-        <div>
-            <button onClick={handleGetReviews}>Load reviews</button>
-            {reviews ? <span>True</span> : <span>False</span>}
-        </div>
-    )
-}
-
-export async function getServerSideProps() {
-    const {data} = await axios('https://my.backend/book')
-
-
-    return {
-        props: {
-            book: data,
-        },
-    }
-}
+export default Home;
