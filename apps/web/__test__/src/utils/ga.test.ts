6438256e2d4b4cfce081e86e7b1022379f3e390d
junho <kode15333@gmail.com>
2023-03-31 23:18:58 +0900

    test: git feat/workflow

--- apps/web/__test__/src/utils/ga.test.ts
+++ apps/web/__test__/src/utils/ga.test.ts
@@ -1,16 +1,6 @@
 import { gaEvent, pageview } from "@/utils/ga";
 
 describe("GA util", () => {
-  it("pageview", () => {
-    const url = "https://www.example.com";
-    window.gtag = jest.fn();
-
-    pageview(url);
-    expect(window.gtag).toHaveBeenCalledWith("config", "GA_TRACKING_ID", {
-      page_path: url,
-    });
-  });
-
   it("event", () => {
     const action = "click";
     const category = "button";
