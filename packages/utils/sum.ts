fd967a4257b36ebe61892da60568a2e898df8b11
Junho You <junho.you@superlabs.us>
2024-04-08 17:11:48 +0900

    chore: git action 수정

--- packages/utils/sum.ts
+++ packages/utils/sum.ts
@@ -1,1 +1,6 @@
 export const sum = (a: number, b: number) => a + b;
+
+
+export const showMobileAlert = () => {
+    alert('mobile');
+}
--- packages/utils2/sum2.ts
+++ packages/utils2/sum2.ts
@@ -1,1 +1,19 @@
 export const sum2 = (a: number, b: number) => a + b;
+
+
+export const sum3 = (a: number, b: number) => a + b;
+
+
+export const sum4 = (a: number, b: number) => a + b;
+
+
+export const getBrowserVersion = () => {
+    const userAgent = navigator.userAgent;
+    const version = userAgent.match(/(MSIE |Trident\/|Edge\/)(\d+)/);
+    if (version) {
+        return parseInt(version[2]);
+    }
+
+
+    return -1;
+}
